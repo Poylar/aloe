e170395f616eb8e1ec132d91532eb6948b48d14b
Гашников Владислав <monsterlpchanel@gmail.com>
2022-12-11 12:51:36 +0300

    update discription

--- src/Layout/Blocks/Description.tsx
+++ src/Layout/Blocks/Description.tsx
@@ -1,21 +1,34 @@
 import React, { FunctionComponent } from 'react';
 
-import descriptionImage from '../../Assets/images/description.jpg';
+import logo from '../../Assets/icons/descriptionLogo.svg';
+import descriptionImage from '../../Assets/images/descriptionCircle.png';
+import useMedia from '../../Hooks/useMedia';
 import { IDescriptionPageData } from '../../Pages/FrontPage/FrontPage.types';
 import styles from './Description.module.scss';
 
 const Description: FunctionComponent<{
   pageData: IDescriptionPageData;
-}> = ({ pageData }) => (
-  <section
-    style={{ backgroundImage: `url(${descriptionImage})` }}
-    className={styles.section}
-  >
-    <div className={styles.section__wrapper}>
-      <h1 className={styles.title}>{pageData.title}</h1>
-      <p className={styles.desc}>{pageData.description}</p>
-    </div>
-  </section>
-);
+}> = ({ pageData }) => {
+  const width = useMedia();
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.section__wrapper}>
+        <h1 className={styles.title}>{pageData.title}</h1>
+        <p className={styles.desc}>{pageData.description}</p>
+      </div>
+
+      {width > 1200 ? (
+        <>
+          <div className={styles.logotype}>
+            <img src={logo} alt='' />
+          </div>
+
+          <img className={styles.circleImage} src={descriptionImage} alt='' />
+        </>
+      ) : null}
+    </section>
+  );
+};
 
 export default Description;
